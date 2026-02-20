fb7dda1b90b9a0657d00b34349aa9b8176502680
Samuel Tomen <113606314+samueltomen@users.noreply.github.com>
2026-02-20 19:41:57 +0100

    add maintenance page

---
+++ src/middleware.ts
@@ -0,0 +1,37 @@
+import {NextResponse} from 'next/server';
+import type {NextRequest} from 'next/server';
+
+export function middleware(request: NextRequest) {
+    const isMaintenanceMode = process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true';
+    const {pathname} = request.nextUrl;
+
+    const isApiRoute = pathname.startsWith('/api/stripe/webhooks');
+    const isStaticAsset = pathname.startsWith('/_next') || pathname.includes('/api/auth');
+    const isMaintenancePage = pathname === '/maintenance';
+    const isPublicFolder = pathname.startsWith('/vercel.png') || pathname.startsWith('/favicon.ico');
+
+    if (isMaintenanceMode) {
+        if (!isMaintenancePage && !isApiRoute && !isStaticAsset && !isPublicFolder) {
+            return NextResponse.redirect(new URL('/maintenance', request.url));
+        }
+    } else {
+        if (isMaintenancePage) {
+            return NextResponse.redirect(new URL('/', request.url));
+        }
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        '/((?!_next/static|_next/image|favicon.ico).*)',
+    ],
+};
